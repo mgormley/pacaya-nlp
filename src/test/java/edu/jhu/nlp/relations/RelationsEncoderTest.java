cd7e0fc45255e61e455f5b9c33e8b8ef256463e4
Matt Gormley <mrg@cs.jhu.edu>
2015-06-01 16:54:39 -0400

    Ignored not-yet-impl test

--- src/test/java/edu/jhu/nlp/relations/RelationsEncoderTest.java
+++ src/test/java/edu/jhu/nlp/relations/RelationsEncoderTest.java
@@ -1,16 +1,17 @@
 package edu.jhu.nlp.relations;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class RelationsEncoderTest {
 
+    @Ignore("Not yet implemented")
     @Test
     public void testAddRelVarAssignments() throws Exception {
         //        RelationsFactorGraphBuilderPrm prm = new RelationsFactorGraphBuilderPrm();
         //        RelationsFactorGraphBuilder rfgb = new RelationsFactorGraphBuilder(prm );
         //        rfgb.build(sent, cj, fg, cs, obsFe);
-        //        RelationsEncoder.addRelVarAssignments(sent, rels, rfgb, vc);
-        throw new RuntimeException("not yet implemented");
+        //        RelationsEncoder.addRelVarAssignments(sent, rels, rfgb, vc);        
     }
 
 }
