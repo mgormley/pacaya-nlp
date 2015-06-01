792298a75d5ded2bf244a805ffb26063f0199ae1
Matt Gormley <mrg@cs.jhu.edu>
2015-06-01 16:35:30 -0400

    Fixing CorpusStatisticsTest

--- src/test/java/edu/jhu/nlp/CorpusStatisticsTest.java
+++ src/test/java/edu/jhu/nlp/CorpusStatisticsTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.assertEquals;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.HashSet;
 import java.util.List;
 
 import org.junit.Test;
@@ -14,6 +15,7 @@ import edu.jhu.nlp.data.conll.CoNLL09ReadWriteTest;
 import edu.jhu.nlp.data.conll.CoNLL09Sentence;
 import edu.jhu.nlp.data.simple.AnnoSentence;
 import edu.jhu.nlp.data.simple.AnnoSentenceCollection;
+import edu.jhu.pacaya.util.collections.Sets;
 
 /**
  * Unit tests for {@link CorpusStatisticsTest}.
@@ -22,23 +24,14 @@ import edu.jhu.nlp.data.simple.AnnoSentenceCollection;
  */
 public class CorpusStatisticsTest {
     
-    String expectedCsToString = "CorpusStatistics [\n"
-            + "     knownWords=[de, ., ,],\n"
-            + "     topNWords=[de],\n"
-            + "     knownUnks=[UNK-LC-s, UNK-CAPS, UNK, UNK-LC],\n"
-            + "     knownPostags=[f, v, d, s, c, r, p, a, n, z],\n"
-            + "     linkStateNames=[True, False],\n"
-            + "     roleStateNames=[argUNK, arg2, arg1, arg0, _, argm],\n"
-            + "     maxSentLength=30]";
-    
     @Test
-    // TODO: This is a hacky way to test for correctness, but fine for a quick check.
     public void testCreation() throws IOException {
         InputStream inputStream = this.getClass().getResourceAsStream(CoNLL09ReadWriteTest.conll2009Example);
         CoNLL09FileReader cr = new CoNLL09FileReader(inputStream);
         CorpusStatisticsPrm csPrm = new CorpusStatisticsPrm();
         csPrm.topN = 1;
         List<CoNLL09Sentence> sents = cr.readSents(4);
+        cr.close();
         AnnoSentenceCollection simpleSents = new AnnoSentenceCollection();
         for (CoNLL09Sentence sent : sents) {
             sent.normalizeRoleNames();
@@ -48,11 +41,12 @@ public class CorpusStatisticsTest {
         CorpusStatistics cs = new CorpusStatistics(csPrm);
         cs.init(simpleSents);
 
-        System.out.println(expectedCsToString);
-        System.out.println();
-        System.out.println(cs);
-        
-        assertEquals(expectedCsToString, cs.toString());
+        assertEquals(Sets.getSet("de", ",", "."), new HashSet<String>(cs.knownWords));
+        assertEquals(Sets.getSet("de"), new HashSet<String>(cs.topNWords));
+        assertEquals(Sets.getSet("UNK-CAPS", "UNK", "UNK-LC", "UNK-LC-s"), new HashSet<String>(cs.knownUnks));
+        assertEquals(Sets.getSet("True", "False"), new HashSet<String>(cs.linkStateNames));
+        assertEquals(Sets.getSet("argUNK", "arg2", "arg1", "arg0", "argm", "_"), new HashSet<String>(cs.roleStateNames));
+        assertEquals(30, cs.maxSentLength);        
     }
 
 }
