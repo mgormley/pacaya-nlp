67d7eb5b5019f45e4b516424c801850705950a7e
Adam Teichert <teichert@jhu.edu>
2016-03-16 15:47:35 -0400

    SRL evaluator now only looking at gold args when structure is PAIRS_GIVEN

--- src/main/java/edu/jhu/nlp/eval/SrlEvaluator.java
+++ src/main/java/edu/jhu/nlp/eval/SrlEvaluator.java
@@ -35,17 +35,20 @@ public class SrlEvaluator extends F1Evaluator implements Evaluator {
         public boolean evalPredSense = true;
         /** Whether to evaluate predicate position. */
         public boolean evalPredPosition = false;
+        /** Whether to evaluate predicate position. */
+        public boolean evalArgPosition = false;
         /** Whether to evaluate arguments (i.e. semantic roles). */
         public boolean evalRoles = true;
         /** Mimic eval09.pl definition of predicate sense equality. */
         public boolean mimicEval09pl = true;
         public boolean evalAmbiguousSrlLabels = false;
         public SrlEvaluatorPrm() { }
-        public SrlEvaluatorPrm(boolean labeled, boolean evalSense, boolean evalPredicatePosition, boolean evalRoles) {
+        public SrlEvaluatorPrm(boolean labeled, boolean evalSense, boolean evalPredicatePosition, boolean evalRoles, boolean evalArgumentPosition) {
             this.labeled = labeled;
             this.evalPredSense = evalSense;
             this.evalPredPosition = evalPredicatePosition;
             this.evalRoles = evalRoles;
+            this.evalArgPosition = evalArgumentPosition;
         }
     }
     
--- src/main/java/edu/jhu/nlp/joint/JointNlpRunner.java
+++ src/main/java/edu/jhu/nlp/joint/JointNlpRunner.java
@@ -496,12 +496,12 @@ public class JointNlpRunner {
             }
             if (CorpusHandler.getPredLatAts().contains(AT.SRL_PRED_IDX)) {
                 // Evaluate F1 of unlabled predicate position identification.
-                eval.add(new SrlEvaluator(new SrlEvaluatorPrm(false, false, predictPredPos, false)));
+                eval.add(new SrlEvaluator(new SrlEvaluatorPrm(false, false, predictPredPos, false, (roleStructure != RoleStructure.PAIRS_GIVEN))));
                 eval.add(new SrlPredIdAccuracy());
             }
             if (CorpusHandler.getPredLatAts().contains(AT.SRL)) {
                 eval.add(new SrlSelfLoops());
-                eval.add(new SrlEvaluator(new SrlEvaluatorPrm(true, predictSense, predictPredPos, (roleStructure != RoleStructure.NO_ROLES))));
+                eval.add(new SrlEvaluator(new SrlEvaluatorPrm(true, predictSense, predictPredPos, (roleStructure != RoleStructure.NO_ROLES), (roleStructure != RoleStructure.PAIRS_GIVEN))));
             }
             if (CorpusHandler.getGoldOnlyAts().contains(AT.SPRL)) {
                 Set<SprlClassLabel> nils = SprlClassLabel.getNils();
