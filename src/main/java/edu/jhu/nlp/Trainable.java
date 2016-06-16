31e5fed47951081486de5da3888a87228fb31a87
Matt Gormley <mgormley@cs.cmu.edu>
2016-06-16 10:45:38 -0400

    Trainable now extends Annotator

--- src/main/java/edu/jhu/nlp/Trainable.java
+++ src/main/java/edu/jhu/nlp/Trainable.java
@@ -2,7 +2,7 @@ package edu.jhu.nlp;
 
 import edu.jhu.nlp.data.simple.AnnoSentenceCollection;
 
-public interface Trainable {
+public interface Trainable extends Annotator {
 
     void train(AnnoSentenceCollection trainInput, AnnoSentenceCollection trainGold, 
             AnnoSentenceCollection devInput, AnnoSentenceCollection devGold);
