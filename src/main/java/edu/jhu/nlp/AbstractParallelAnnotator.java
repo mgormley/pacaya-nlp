0011d591d1fedbdcb24dbc820ec953f9afad5a00
Adam Poliak <azpoliak@gmail.com>
2016-08-17 11:40:06 -0400

    AbstractParallelAnnotator: reverted the addition of parallel and changes to annotate(). Surround log.trace() calles with checks to determine if traceIsEnabled

--- src/main/java/edu/jhu/nlp/AbstractParallelAnnotator.java
+++ src/main/java/edu/jhu/nlp/AbstractParallelAnnotator.java
@@ -14,33 +14,20 @@ public abstract class AbstractParallelAnnotator implements Annotator {
 
     private static final Logger log = LoggerFactory.getLogger(AbstractParallelAnnotator.class);
     private static final long serialVersionUID = 1L;
-    // This is for debugging purposes; if you want a stack trace that
-    // goes back to main, you can turn parallel off
-    private boolean parallel = true;
-
-    private void wrappedAnnotate(AnnoSentence sent) {
-        try {
-            annotate(sent);
-        } catch (Throwable t) {
-            AbstractParallelAnnotator.logThrowable(log, t);
-        }
-    }
 
     @Override
     public void annotate(final AnnoSentenceCollection sents) {
-        // Add the new predictions to each sentence.
-        if (parallel) {
-            Threads.forEach(0, sents.size(), new FnIntToVoid() {            
-                    @Override
-                    public void call(int i) {
-                        wrappedAnnotate(sents.get(i));
+    // Add the new predictions to each sentence.
+        Threads.forEach(0, sents.size(), new FnIntToVoid() {
+                @Override
+                public void call(int i) {
+                    try {
+                        annotate(sents.get(i));
+                    } catch (Throwable t) {
+                        AbstractParallelAnnotator.logThrowable(log, t);
                     }
-                });
-        } else {
-            for (int i = 0; i < sents.size(); i++) {
-                wrappedAnnotate(sents.get(i));
-            }
-        }
+                }
+            });
     }
 
     public abstract void annotate(AnnoSentence sent);
@@ -50,8 +37,9 @@ public abstract class AbstractParallelAnnotator implements Annotator {
         PrintWriter pW = new PrintWriter(sW);
         t.printStackTrace(pW);
         String msg = sW.getBuffer().toString();
-        //String msg = (t.getMessage() == null) ? "" : ": " + t.getMessage();
-        log.error("Failed to annotate sentence. Caught throwable: " + t.getClass() + msg);
-        log.trace("Stacktrace from previous ERROR:\n", t);
+        if (log.isTraceEnabled()) {
+            log.trace("Failed to annotate sentence. Caught throwable: " + t.getClass() + msg);
+            log.trace("Stacktrace from previous ERROR:\n", t);
+        }
     }
 }
